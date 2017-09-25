e61c77031e6f7041ccf60ab912a7665de308ec8f
aflattminor <ajflattery50@gmail.com>
2017-09-24 22:23:59 -0400

    react router added

---
+++ react/src/containers/UserShowContainer.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+//
+// class UserShowContainer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//
+//     }
+//   }
+//
+//   render() {
+//
+//     return (
+//       <div className="UserShowContainer">
+//       </div>
+//     );
+//   }
+// }
+//
+// export default UserShowContainer;
